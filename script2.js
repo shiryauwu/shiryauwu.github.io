7298e9b58bb66a390f199675ec0e7da867a02747
shiryauwu <70473372+shiryauwu@users.noreply.github.com>
2023-05-23 00:53:05 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,9 @@
-/*ymaps.ready(init)
+ymaps.ready(init)
 
 
 function init() {
     let map = new ymaps.Map('map', {
-        center: [55.82639251104599, 49.148980333393254],
+        center: [55.78989998447551, 49.13523000377707],
         zoom: 15
     });
     map.controls.remove('searchControl');
@@ -11,10 +11,10 @@ function init() {
     map.controls.remove('searchControl');
     map.behaviors.disable(['scrollZoom']);
 
-    let placemark = new ymaps.Placemark([55.82639251104599, 49.148980333393254], {
-        balloonContentHeader: 'Трейд-Модус',
-        balloonContentBody: 'Тц Савиново, 3 этаж',
-        balloonContentFooter: 'Ямашева 93'
+    let placemark = new ymaps.Placemark([55.79045611267339, 49.13424295085957], {
+        balloonContentHeader: 'Партия новый маршрут',
+        balloonContentBody: 'университет "ТИСБИ", 5 этаж',
+        balloonContentFooter: 'Муштари 11'
     }, {
         iconLayout: 'default#image',
         iconImageHref: 'images/map.png',
@@ -25,7 +25,7 @@ function init() {
 
     map.geoObjects.add(placemark);
 }
-*/
+
 
 $(document).ready(function () {
     let header = $('.header')
